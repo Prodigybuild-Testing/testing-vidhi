6f50d5a54885d8df6bd716e4ca1273a16877bbae
Vidhi-TEST <test9.vidhi@gmail.com>
2024-12-12 16:09:19 +0530

    SCP-2 (bug) [SonarQube] Access of 'yarr' at index 2, while it holds only 2 'const char *' elements

--- c/linearsearch.c
+++ c/linearsearch.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 
-int linsearch(const char **yarr, const char *val)
+int linsearch(const char **yarr, const char *val, int size)
 {
-    for(int i = 0; i < sizeof(yarr); i++)
+    for(int i = 0; i < size; i++)
     {
         if(strcmp(yarr[i], val) == 0)
         {
@@ -18,6 +18,7 @@ int main()
 {
     const char *yarr[] = {"Shluck much", "much Shluck"};
     const char *val = "Shluck Much";
-    linsearch(yarr, val);
+    int size = sizeof(yarr) / sizeof(yarr[0]);
+    linsearch(yarr, val, size);
     return 0;
 }
